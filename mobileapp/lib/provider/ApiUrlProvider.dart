692f02a3d8202a1ce440b3ad0d8f3358e5e70002
joseph rana <josephranamgr7@gmail.com>
2025-08-21 13:48:17 +0545

    chore: update api uril provider

--- mobileapp/lib/provider/ApiUrlProvider.dart
+++ mobileapp/lib/provider/ApiUrlProvider.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ApiUrlProvider with ChangeNotifier {
-  // String _apiUri = "https://deltasign.sireto.io";
-  String _apiUri = "http://192.168.0.247:8000";
+  String _apiUri = "https://deltasign.sireto.io";
+  // String _apiUri = "http://192.168.0.247:8000";
   String get apiuri => _apiUri;
   void setApiUri(String apiUri) {
     _apiUri = apiUri;
